8e4edc412b6a9aa0a0da860d213399eb1e5237d4
ilya0 <iosovets@gmail.com>
2019-09-30 11:01:18 -0700

    added node stuff

--- amazon/binarytree.js
+++ amazon/binarytree.js
@@ -3,7 +3,7 @@ var tree; //tree structure
 // run script
 function setup(){
     noCanvas();
-    tree = new TreeWalker();
+     tree = new Tree();
     var n = new Node(5); 
     tree.addNode(n);
     
@@ -16,6 +16,7 @@ function Tree(){
         this.root = null;
     }  
 
+//creating a child node
 function Node(val){
         this.value = val;
         this.left = null;
@@ -23,9 +24,10 @@ function Node(val){
     }
 
 
-
+//tree object has a function that will create a child node
     Tree.prototype.addNode = function(n){
       var n = new Node(val);
+
         if(this.root == null){
             this.root = n;
         }else{
@@ -34,18 +36,47 @@ function Node(val){
 
       }
 
-      Node.prototype.addNode = function(n){
-          if(n.value < this.value){
-              if(this.left == null){
-                this.left = n;
-              }else{
-                  this.left.addNode(n);
-              }
-              
-          }else{
-              this.right.addNode(n);
 
-          }
-      }
+      //node functionality  - recursion
+
+    Node.prototype.addNode = function(n){
+
+          if(n.value < this.value){
+
+                if(this.left == null){ //if left node doesnt exist to this node, set n to left node
+                    this.left = n;
+                }else{
+                    this.left.addNode(n) // if node exists add node
+            }
+
+        }else if (n.value > this,value){
+                if(this.right == null){
+                    this.right = n;
+                }else{
+                    this.right.addNode(n);
+                }
+                    
+        }
+              
+    }
+
+
+/// adding prototype visit to node
+Node.prototype.visit = function(){
+    if(this.left != null){
+        this.left.visit();        
+    }
+
+    console.log(this.value);
+    if(this.right != null){
+        this.right.visit();
+    }
+}
+
+    //search the tree
+    Tree.prototype.traverse = function(){
+        this.root.visit
+    }
+      
     
 
