c3e73b5d1c384c749cb0dc9756e57d619354cb0a
ilya0 <iosovets@gmail.com>
2019-09-30 09:38:08 -0700

    added more code

--- amazon/binarytree.js
+++ amazon/binarytree.js
@@ -1,8 +1,10 @@
-var tree;
+var tree; //tree structure
+
+// run script
 function setup(){
     noCanvas();
     tree = new TreeWalker();
-    var n = new Node(5); // run test
+    var n = new Node(5); 
     tree.addNode(n);
     
     console.log(tree);
@@ -12,10 +14,38 @@ function setup(){
 // root node
 function Tree(){
         this.root = null;
+    }  
+
+function Node(val){
+        this.value = val;
+        this.left = null;
+        this.right = null;
     }
 
+
+
     Tree.prototype.addNode = function(n){
+      var n = new Node(val);
         if(this.root == null){
             this.root = n;
+        }else{
+            this.root.addNode(n);
         }
-    }
+
+      }
+
+      Node.prototype.addNode = function(n){
+          if(n.value < this.value){
+              if(this.left == null){
+                this.left = n;
+              }else{
+                  this.left.addNode(n);
+              }
+              
+          }else{
+              this.right.addNode(n);
+
+          }
+      }
+    
+
